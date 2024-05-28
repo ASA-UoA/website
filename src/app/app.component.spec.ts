8a50768b4e865c791ff084e9144c50bc7b71f761
JoshuaMorley <joshua.morley31@gmail.com>
2024-05-28 17:18:26 +1200

    Removed some tests from template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'asa-website' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('asa-website');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, asa-website');
-  });
 });
