015e43ea7b517fd2380ab1727186ffd8cf2174a0
JoshuaMorley <joshua.morley31@gmail.com>
2024-07-14 19:31:43 +1200

    added all event view

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,11 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ApiService} from '../../services/api.service';
+import {DatePipe} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    RouterLink
+  ],
   templateUrl: './events.component.html',
 })
 export class EventsComponent {
-
+  api = inject(ApiService);
 }
