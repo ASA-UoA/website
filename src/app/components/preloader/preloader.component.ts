a13ca942a5621cdd35732cfbf7042ad893a5beb7
JoshuaMorley <joshua.morley31@gmail.com>
2024-07-14 20:12:30 +1200

    changed banner

--- src/app/components/preloader/preloader.component.ts
+++ src/app/components/preloader/preloader.component.ts
@@ -6,5 +6,5 @@ import {Component, HostBinding} from '@angular/core';
   templateUrl: 'preloader.component.html'
 })
 export class PreloaderComponent {
-  @HostBinding('class') class = 'h-full w-full absolute top-0 left-0 bg-red-500 grid place-items-center'
+  @HostBinding('class') class = 'h-full w-full absolute top-0 left-0 bg-black grid place-items-center'
 }
