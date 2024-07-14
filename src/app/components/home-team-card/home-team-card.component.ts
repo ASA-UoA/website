6e507d836a5e00595be55190818cf03dcb1f6032
JoshuaMorley <joshua.morley31@gmail.com>
2024-07-15 00:32:18 +1200

    added the team page

--- src/app/components/home-team-card/home-team-card.component.ts
+++ src/app/components/home-team-card/home-team-card.component.ts
@@ -1,14 +1,17 @@
 import {Component, HostBinding, input} from '@angular/core';
 import {Team} from "../../types/events";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-home-team-card',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './home-team-card.component.html',
   styles: ``
 })
 export class HomeTeamCardComponent {
   teamMember = input.required<Team>()
-  @HostBinding('class') classes = 'text-center';
+  @HostBinding('class') classes = 'text-center w-full sm:w-auto';
 }
--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,11 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
+import {ApiService} from '../../services/api.service';
+import {HomeTeamCardComponent} from "../../components/home-team-card/home-team-card.component";
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeTeamCardComponent
+  ],
   templateUrl: './team.component.html',
 })
 export class TeamComponent {
 
+  api = inject(ApiService);
+
+  executives = computed(() => {
+    return this.api.team().filter(member => member.exec);
+  });
+
+  committee = computed(() => {
+    return this.api.team().filter(member => !member.exec);
+  });
 }
